b189e40275690077fe84bf8e27df5b16bd6cad27
Dany Ponce <dponce@dezain.com.pe>
2026-07-17 16:28:05 -0500

    Modificacion sale controller

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -273,7 +273,7 @@ class SaleController extends Controller
             $printer->setJustification(Printer::JUSTIFY_CENTER);
             $printer->setEmphasis(true);
             $printer->setFont(Printer::FONT_A);
-            $printer->text("CONCESIONARIO DE ALIMENTOS LUCEMIR\n");
+            $printer->text("CONCESIONARIO DE ALIMENTOS DELICIOUS FOOD.\n");
             $printer->text("\n");
             $printer->setFont(Printer::FONT_B);
             $printer->setEmphasis(false);
